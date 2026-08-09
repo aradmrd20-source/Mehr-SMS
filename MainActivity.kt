8c80c2d0a8dd9d44dca4b9782ecaec33c556a040
aradmrd20-source <aradmrd20@gmail.com>
2026-08-09 14:21:14 +0330

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mehrsms
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // کد اصلی برنامه
+    }
+}
+
