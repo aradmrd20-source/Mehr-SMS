9dd49a4d2962c931a2b3c85eea82c22c2dc56e49
aradmrd20-source <aradmrd20@gmail.com>
2026-08-09 15:43:57 +0330

    Create SmsReceiver.kt

---
+++ SmsReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.mehrsms
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // دریافت پیامک در پس‌زمینه
+    }
+}
+
